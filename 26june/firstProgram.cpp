0c5a5fe33ee6048b277c37466717d0e4e0108276
Abed akhtar <abedakhtar02@gmail.com>
2024-06-27 00:27:26 +0530

    firstProgram done

---
+++ 26june/firstProgram.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   /* cout << "namaste!" << endl; 
+    cout << "enter a number\n";
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << "sum is"<<a+b<<endl;
+    int size = sizeof(a);
+    cout <<"size of a is"<< size<<endl;
+
+    int a = -99;
+    cout<< a<<endl;
+
+    char c = 99;
+    cout<< c <<endl;
+*/
+    
+
+
+
+}
