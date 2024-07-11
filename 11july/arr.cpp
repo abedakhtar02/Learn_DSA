519f75e88cb5b1a6e4c841d908d5d18302f97fdb
Abed akhtar <abedakhtar02@gmail.com>
2024-07-12 02:28:18 +0530

    Array

---
+++ 11july/arr.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n,one[10] ;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        one[i]=1;
+        cout<< one[i]<<" ";
+    }
+
+}
